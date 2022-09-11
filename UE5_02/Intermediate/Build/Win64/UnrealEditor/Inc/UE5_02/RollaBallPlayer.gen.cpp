4564c6bc5e5a9df8220addec87502662eb6eec43
Emka5885 <masiak-emilia@wp.pl>
2022-09-11 12:37:06 +0200

    Do 00.34.24

--- UE5_02/Intermediate/Build/Win64/UnrealEditor/Inc/UE5_02/RollaBallPlayer.gen.cpp
+++ UE5_02/Intermediate/Build/Win64/UnrealEditor/Inc/UE5_02/RollaBallPlayer.gen.cpp
@@ -13,6 +13,9 @@ void EmptyLinkFunctionForGeneratedCodeRollaBallPlayer() {}
 	UE5_02_API UClass* Z_Construct_UClass_ARollaBallPlayer();
 	ENGINE_API UClass* Z_Construct_UClass_APawn();
 	UPackage* Z_Construct_UPackage__Script_UE5_02();
+	ENGINE_API UClass* Z_Construct_UClass_UStaticMeshComponent_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_USpringArmComponent_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_UCameraComponent_NoRegister();
 // End Cross Module References
 	void ARollaBallPlayer::StaticRegisterNativesARollaBallPlayer()
 	{
@@ -28,6 +31,31 @@ void EmptyLinkFunctionForGeneratedCodeRollaBallPlayer() {}
 #if WITH_METADATA
 		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
 #endif
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_Mesh_MetaData[];
+#endif
+		static const UECodeGen_Private::FObjectPropertyParams NewProp_Mesh;
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_SpringArm_MetaData[];
+#endif
+		static const UECodeGen_Private::FObjectPropertyParams NewProp_SpringArm;
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_Camera_MetaData[];
+#endif
+		static const UECodeGen_Private::FObjectPropertyParams NewProp_Camera;
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_MoveForce_MetaData[];
+#endif
+		static const UECodeGen_Private::FFloatPropertyParams NewProp_MoveForce;
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_JumpImpulse_MetaData[];
+#endif
+		static const UECodeGen_Private::FFloatPropertyParams NewProp_JumpImpulse;
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_MaxJumpCount_MetaData[];
+#endif
+		static const UECodeGen_Private::FIntPropertyParams NewProp_MaxJumpCount;
+		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
 		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
 		static const UECodeGen_Private::FClassParams ClassParams;
 	};
@@ -42,6 +70,65 @@ void EmptyLinkFunctionForGeneratedCodeRollaBallPlayer() {}
 		{ "ModuleRelativePath", "Game/RollaBallPlayer.h" },
 	};
 #endif
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Mesh_MetaData[] = {
+		{ "Category", "Components" },
+		{ "Comment", "// Definite Components\n" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "Game/RollaBallPlayer.h" },
+		{ "ToolTip", "Definite Components" },
+	};
+#endif
+	const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Mesh = { "Mesh", nullptr, (EPropertyFlags)0x00200800000a001d, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(ARollaBallPlayer, Mesh), Z_Construct_UClass_UStaticMeshComponent_NoRegister, METADATA_PARAMS(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Mesh_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Mesh_MetaData)) };
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_SpringArm_MetaData[] = {
+		{ "Category", "Components" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "Game/RollaBallPlayer.h" },
+	};
+#endif
+	const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_SpringArm = { "SpringArm", nullptr, (EPropertyFlags)0x00200800000a001d, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(ARollaBallPlayer, SpringArm), Z_Construct_UClass_USpringArmComponent_NoRegister, METADATA_PARAMS(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_SpringArm_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_SpringArm_MetaData)) };
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Camera_MetaData[] = {
+		{ "Category", "Components" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "Game/RollaBallPlayer.h" },
+	};
+#endif
+	const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Camera = { "Camera", nullptr, (EPropertyFlags)0x00200800000a001d, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(ARollaBallPlayer, Camera), Z_Construct_UClass_UCameraComponent_NoRegister, METADATA_PARAMS(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Camera_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Camera_MetaData)) };
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MoveForce_MetaData[] = {
+		{ "Category", "RollaBallPlayer" },
+		{ "Comment", "// VARIABLES //\n" },
+		{ "ModuleRelativePath", "Game/RollaBallPlayer.h" },
+		{ "ToolTip", "VARIABLES" },
+	};
+#endif
+	const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MoveForce = { "MoveForce", nullptr, (EPropertyFlags)0x0020080000000005, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(ARollaBallPlayer, MoveForce), METADATA_PARAMS(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MoveForce_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MoveForce_MetaData)) };
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_JumpImpulse_MetaData[] = {
+		{ "Category", "RollaBallPlayer" },
+		{ "Comment", "//T?umaczenie - (force) \"si?a\"\n" },
+		{ "ModuleRelativePath", "Game/RollaBallPlayer.h" },
+		{ "ToolTip", "T?umaczenie - (force) \"si?a\"" },
+	};
+#endif
+	const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_JumpImpulse = { "JumpImpulse", nullptr, (EPropertyFlags)0x0020080000000005, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(ARollaBallPlayer, JumpImpulse), METADATA_PARAMS(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_JumpImpulse_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_JumpImpulse_MetaData)) };
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MaxJumpCount_MetaData[] = {
+		{ "Category", "RollaBallPlayer" },
+		{ "ModuleRelativePath", "Game/RollaBallPlayer.h" },
+	};
+#endif
+	const UECodeGen_Private::FIntPropertyParams Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MaxJumpCount = { "MaxJumpCount", nullptr, (EPropertyFlags)0x0020080000000005, UECodeGen_Private::EPropertyGenFlags::Int, RF_Public|RF_Transient|RF_MarkAsNative, 1, STRUCT_OFFSET(ARollaBallPlayer, MaxJumpCount), METADATA_PARAMS(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MaxJumpCount_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MaxJumpCount_MetaData)) };
+	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_ARollaBallPlayer_Statics::PropPointers[] = {
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Mesh,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_SpringArm,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_Camera,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MoveForce,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_JumpImpulse,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ARollaBallPlayer_Statics::NewProp_MaxJumpCount,
+	};
 	const FCppClassTypeInfoStatic Z_Construct_UClass_ARollaBallPlayer_Statics::StaticCppClassTypeInfo = {
 		TCppClassTypeTraits<ARollaBallPlayer>::IsAbstract,
 	};
@@ -51,11 +138,11 @@ void EmptyLinkFunctionForGeneratedCodeRollaBallPlayer() {}
 		&StaticCppClassTypeInfo,
 		DependentSingletons,
 		nullptr,
-		nullptr,
+		Z_Construct_UClass_ARollaBallPlayer_Statics::PropPointers,
 		nullptr,
 		UE_ARRAY_COUNT(DependentSingletons),
 		0,
-		0,
+		UE_ARRAY_COUNT(Z_Construct_UClass_ARollaBallPlayer_Statics::PropPointers),
 		0,
 		0x009000A4u,
 		METADATA_PARAMS(Z_Construct_UClass_ARollaBallPlayer_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_ARollaBallPlayer_Statics::Class_MetaDataParams))
@@ -78,9 +165,9 @@ void EmptyLinkFunctionForGeneratedCodeRollaBallPlayer() {}
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
 	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_UE5_02_Source_UE5_02_Game_RollaBallPlayer_h_Statics::ClassInfo[] = {
-		{ Z_Construct_UClass_ARollaBallPlayer, ARollaBallPlayer::StaticClass, TEXT("ARollaBallPlayer"), &Z_Registration_Info_UClass_ARollaBallPlayer, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ARollaBallPlayer), 1427334641U) },
+		{ Z_Construct_UClass_ARollaBallPlayer, ARollaBallPlayer::StaticClass, TEXT("ARollaBallPlayer"), &Z_Registration_Info_UClass_ARollaBallPlayer, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ARollaBallPlayer), 104820994U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_UE5_02_Source_UE5_02_Game_RollaBallPlayer_h_1439573878(TEXT("/Script/UE5_02"),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_UE5_02_Source_UE5_02_Game_RollaBallPlayer_h_2476891422(TEXT("/Script/UE5_02"),
 		Z_CompiledInDeferFile_FID_UE5_02_Source_UE5_02_Game_RollaBallPlayer_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_UE5_02_Source_UE5_02_Game_RollaBallPlayer_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
--- UE5_02/Source/UE5_02/Game/RollaBallPlayer.cpp
+++ UE5_02/Source/UE5_02/Game/RollaBallPlayer.cpp
@@ -2,12 +2,26 @@
 
 
 #include "RollaBallPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ARollaBallPlayer::ARollaBallPlayer()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Create Components - Before this step the won't exist on our Actor, they've only been defined.
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	// Set the Root Component to be our Mesh.
+	RootComponent = Mesh;
+	// Attach the SpringArm to Mesh, Spring will now follow the Mesh transform.   //T³umaczenie - (attach) "przytwierdzaæ"
+	SpringArm->SetupAttachment(Mesh);
+	// Attach the Camera to the SpringArm, Camera will now follow the SpringArm transform.
+	Camera->SetupAttachment(SpringArm);
 
 }
 
@@ -18,17 +32,25 @@ void ARollaBallPlayer::BeginPlay()
 	
 }
 
-// Called every frame
-void ARollaBallPlayer::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
 // Called to bind functionality to input
 void ARollaBallPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ARollaBallPlayer::MoveRight(float Value)
+{
+
+}
+
+void ARollaBallPlayer::MoveForward(float Value)
+{
+
+}
+
+void ARollaBallPlayer::Jump()
+{
 
 }
 
